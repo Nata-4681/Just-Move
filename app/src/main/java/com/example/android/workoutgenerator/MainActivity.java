d83973d144341ad04e55b2032745530518c49e23
Cole Clark <clark.cole@husky.neu.edu>
2018-03-08 15:11:24 -0500

    Updated the UI to include a background image. Also add styling to the Spinners and main page. Changed the name of the app to 'Move'

--- app/src/main/java/com/example/android/workoutgenerator/MainActivity.java
+++ app/src/main/java/com/example/android/workoutgenerator/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -19,14 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner equipSpinner1 = (Spinner) findViewById(R.id.equipment_spinner1);
+        Spinner equipSpinner2 = (Spinner) findViewById(R.id.equipment_spinner2);
+        Spinner equipSpinner3 = (Spinner) findViewById(R.id.equipment_spinner3);
+        ArrayAdapter equipAdapter = ArrayAdapter.createFromResource(this,
+                R.array.equipment_array, R.layout.spinner_item);
+
+        equipAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        equipSpinner1.setAdapter(equipAdapter);
+        equipSpinner2.setAdapter(equipAdapter);
+        equipSpinner3.setAdapter(equipAdapter);
+
+        Spinner moveSpinner1 = (Spinner) findViewById(R.id.movements_spinner1);
+        Spinner moveSpinner2 = (Spinner) findViewById(R.id.movements_spinner2);
+        Spinner moveSpinner3 = (Spinner) findViewById(R.id.movements_spinner3);
+        ArrayAdapter moveAdapter = ArrayAdapter.createFromResource(this,
+                R.array.movement_array, R.layout.spinner_item);
+
+        moveAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        moveSpinner1.setAdapter(moveAdapter);
+        moveSpinner2.setAdapter(moveAdapter);
+        moveSpinner3.setAdapter(moveAdapter);
+
+
 
     }
 
 
     public void submit(View view){
         Intent intent = new Intent(this, WorkoutActivity.class);
-        Spinner equipSpinner1 = (Spinner) findViewById(R.id.equipment_spinner);
-        Spinner movementSpinner1 = (Spinner) findViewById(R.id.movements_spinner);
+        Spinner equipSpinner1 = (Spinner) findViewById(R.id.equipment_spinner1);
+        Spinner movementSpinner1 = (Spinner) findViewById(R.id.movements_spinner1);
         String equipSelection1 = equipSpinner1.getSelectedItem().toString();
         String movementSelection1 = movementSpinner1.getSelectedItem().toString();
         intent.putExtra("EquipSelection1", equipSelection1);
