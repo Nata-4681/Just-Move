623a9ccf5cca4330bdd5df7735e08086f18efd7d
Cole Clark <clark.cole@husky.neu.edu>
2018-03-20 13:51:21 -0400

    Added the Remove buttons to the main screen. Also updated the name of the app

--- app/src/main/java/com/example/android/workoutgenerator/MainActivity.java
+++ app/src/main/java/com/example/android/workoutgenerator/MainActivity.java
@@ -35,24 +35,31 @@ public class MainActivity extends AppCompatActivity {
         Spinner equipSpinner1 = (Spinner) findViewById(R.id.equipment_spinner1);
         Spinner equipSpinner2 = (Spinner) findViewById(R.id.equipment_spinner2);
         Spinner equipSpinner3 = (Spinner) findViewById(R.id.equipment_spinner3);
-        ArrayAdapter equipAdapter = ArrayAdapter.createFromResource(this,
-                R.array.equipment_array, R.layout.spinner_item);
+        ArrayAdapter equipAdapter1 = ArrayAdapter.createFromResource(this,
+                R.array.equipment_array1, R.layout.spinner_item);
 
-        equipAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        equipSpinner1.setAdapter(equipAdapter);
-        equipSpinner2.setAdapter(equipAdapter);
-        equipSpinner3.setAdapter(equipAdapter);
+        ArrayAdapter equipAdapter2 = ArrayAdapter.createFromResource(this,
+                R.array.equipment_array2, R.layout.spinner_item);
+
+        equipAdapter1.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        equipAdapter2.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        equipSpinner1.setAdapter(equipAdapter1);
+        equipSpinner2.setAdapter(equipAdapter2);
+        equipSpinner3.setAdapter(equipAdapter2);
 
         Spinner moveSpinner1 = (Spinner) findViewById(R.id.movements_spinner1);
         Spinner moveSpinner2 = (Spinner) findViewById(R.id.movements_spinner2);
         Spinner moveSpinner3 = (Spinner) findViewById(R.id.movements_spinner3);
-        ArrayAdapter moveAdapter = ArrayAdapter.createFromResource(this,
-                R.array.movement_array, R.layout.spinner_item);
+        ArrayAdapter moveAdapter1 = ArrayAdapter.createFromResource(this,
+                R.array.movement_array1, R.layout.spinner_item);
+        ArrayAdapter moveAdapter2 = ArrayAdapter.createFromResource(this,
+                R.array.movement_array2, R.layout.spinner_item);
 
-        moveAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-        moveSpinner1.setAdapter(moveAdapter);
-        moveSpinner2.setAdapter(moveAdapter);
-        moveSpinner3.setAdapter(moveAdapter);
+        moveAdapter1.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        moveAdapter2.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        moveSpinner1.setAdapter(moveAdapter1);
+        moveSpinner2.setAdapter(moveAdapter2);
+        moveSpinner3.setAdapter(moveAdapter2);
 
         mDbHelper = new WorkoutDbHelper(this);
 
@@ -98,6 +105,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void removeEquipment(View view){
+
+        LinearLayout spinner2 = (LinearLayout) findViewById(R.id.equipment_layout2);
+        LinearLayout spinner3 = (LinearLayout) findViewById(R.id.equipment_layout3);
+
+        if (equipCount == 3){
+            spinner3.setVisibility(View.GONE);
+            equipCount -= 1;
+        }
+        else if (equipCount == 2){
+            spinner2.setVisibility(View.GONE);
+            equipCount -= 1;
+        }
+
+    }
+
     /**
      * Makes the second and third 'movements' spinners visible on the app
      * @param view
@@ -115,6 +138,22 @@ public class MainActivity extends AppCompatActivity {
         else if (movementCount == 2){
             spinner3.setVisibility(View.VISIBLE);
             movementCount += 1;
+        }
+
+    }
+
+    public void removeMovement(View view){
+        LinearLayout spinner2 = (LinearLayout) findViewById(R.id.movement_layout2);
+        LinearLayout spinner3 = (LinearLayout) findViewById(R.id.movement_layout3);
+
+        if (movementCount == 3){
+            spinner3.setVisibility(View.GONE);
+            movementCount -= 1;
+        }
+
+        else if (movementCount == 2){
+            spinner2.setVisibility(View.GONE);
+            movementCount -= 1;
         }
 
     }
