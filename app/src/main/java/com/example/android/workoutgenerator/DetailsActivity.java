c9a501f7a262dd8bc818aacf4e2c17b11e44a030
Cole Clark <clark.cole@husky.neu.edu>
2018-03-20 12:33:19 -0400

    Ripple effect added to all buttons including the GridView items

--- app/src/main/java/com/example/android/workoutgenerator/DetailsActivity.java
+++ app/src/main/java/com/example/android/workoutgenerator/DetailsActivity.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -17,7 +18,6 @@ import com.example.android.workoutgenerator.data.WorkoutContract.WorkoutEntry;
 import com.example.android.workoutgenerator.data.WorkoutDbHelper;
 
 public class DetailsActivity extends AppCompatActivity {
-
 
 
     @Override
