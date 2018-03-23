1c9971cd9bc61919a55bdaec6b5447b48cad459e
Cole Clark <clark.cole@husky.neu.edu>
2018-03-23 17:28:12 -0400

    Changed the maing page font color to match the yellow used in other screens. Add ripple effects for the spinners (both the items and the drop downs)

--- app/src/main/java/com/example/android/workoutgenerator/SplashActivity.java
+++ app/src/main/java/com/example/android/workoutgenerator/SplashActivity.java
@@ -10,8 +10,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
